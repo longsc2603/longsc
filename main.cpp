e8eef03a8a4c98eee823d1ab44bb31cf58c8618f
longsc2603 <56826526+longsc2603@users.noreply.github.com>
2019-11-15 21:45:03 +0700

    Add files via upload
    Rename main.cpp to Bài in số a Vinh

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n,a,b,c;
+    b=1;c=1;
+    cout<<"Nhập số dòng : ";cin>>n;
+    while(c<=n) {
+        a=c;b=1;
+        while (b<=c) {
+            cout<<(a++%10);
+            b++;
+        }
+        b=b-2;a=a-2;
+        while (b>=1) {
+            cout<<(a--%10);
+            b--;
+        }
+        cout<<endl;
+        c++;
+    }
+    return 0;
+}
+
